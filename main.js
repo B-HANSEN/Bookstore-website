eb5143671ba6c9d34850b7163a5dfe093c9a58ff
Bjoern Hansen <b.hansen.ubiqum@gmail.com>
2019-06-28 15:18:34 +0200

    search field added:

--- main.js
+++ main.js
@@ -1,42 +1,56 @@
 Vue.config.devtools = true;
 
-
 Vue.component('books-comp', {
     props: ['books'],
     template: `
-    <div id="container" class="d-flex flex-wrap justify-content-around">
-    <div class="flip-card" v-for="book in books">
-        <div class="flip-card-intermediate">
-            <div class="flip-card-inner">
-                
-                <div class="flip-card-front">
-                    <img v-bind:src="book.portada"/>
-                </div>
+<div>
+    <input
+    type="text"
+    class="searchfield"
+    v-model="search" 
+    placeholder="Search..." />
 
-                <div class="flip-card-back">
-                    <h5> {{ book.titulo }} </h5>
-                    <p> {{ book.descripcion }} </p>
-                    <a data-fancybox="gallery" :href="book.detalle">
-                    <button>more info
-                    </button>
-                    </a>
+    <div id="container" class="d-flex flex-wrap justify-content-around">
+        <div class="flip-card" v-for="book in filteredBooks"> 
+            <div class="flip-card-intermediate">
+                <div class="flip-card-inner">
+                    
+                    <div class="flip-card-front">
+                        <img :src="book.portada"/>
+                    </div>
+
+                    <div class="flip-card-back">
+                        <h5> {{ book.titulo }} </h5>
+                        <p> {{ book.descripcion }} </p>
+                        <a data-fancybox="gallery" :href="book.detalle">
+                            <button>more info
+                            </button>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-            `
+</div>`,
+    data() {
+        return {
+            search: ""
+        }
+    },
+    computed: {
+        filteredBooks: function () {
+            return this.books.filter((book) => {
+                return book.titulo.toLowerCase().includes(this.search.toLowerCase())
+            })
+        }
+    }
 });
-
-// for search engine 
-// @keyup=""
-
 
 // fetch data from remote, convert to json, store data in variable books
 var app = new Vue({
     el: '#app',
     data: {
-        json: null // initialise empty array
+        json: [] // initialise empty array
     },
     created: function () {
         fetch("https://api.myjson.com/bins/1h3vb3")
@@ -47,22 +61,3 @@ var app = new Vue({
             })
     }
 });
-
-
-
-// more/less button from TGIF-Project
-// var app = new Vue({
-//     el: '#app',
-//     data: {
-//         button: {
-//             text: 'Show More'
-//         },
-//         showPara: true
-//     },
-//     methods: {
-//         hideShow() {
-//             app.showPara = !app.showPara;
-//             app.button.text = app.showPara ? 'Show more' : 'Show less';
-//         }
-//     }
-// });
